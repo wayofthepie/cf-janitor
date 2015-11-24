2db98dce63d9e5e40b0440b8d8a98f54a4d9df48
Stephen O'Brien <wayofthepie@gmail.com>
2015-11-24 21:30:04 +0000

    Fix formatting

--- janitor.go
+++ janitor.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
-	"fmt"
-	"strings"
-	"time"
-	"github.com/cloudfoundry/cli/cf/api/resources"
-	//"github.com/cloudfoundry/cli/cf/terminal"
-	"github.com/cloudfoundry/cli/plugin"
+    "encoding/json"
+    "flag"
+    "fmt"
+    "strings"
+    "time"
+    "github.com/cloudfoundry/cli/cf/api/resources"
+    //"github.com/cloudfoundry/cli/cf/terminal"
+    "github.com/cloudfoundry/cli/plugin"
 
 )
 
 type JanitorPlugin struct{
-	cliConnection plugin.CliConnection
-	before 		  *string
+    cliConnection plugin.CliConnection
+    before 		  *string
 }
 
 /*
@@ -32,51 +32,51 @@ type JanitorPlugin struct{
 *	1 should the plugin exits nonzero.
  */
 func (c *JanitorPlugin) Run(cliConnection plugin.CliConnection, args []string) {
-	c.cliConnection = cliConnection
+    c.cliConnection = cliConnection
 
-	fs := new(flag.FlagSet)
-	c.before = fs.String("before", "", "")
-	fs.Parse(args[1:])
-	c.execute()
+    fs := new(flag.FlagSet)
+    c.before = fs.String("before", "", "")
+    fs.Parse(args[1:])
+    c.execute()
 
 }
 
 
 func (c *JanitorPlugin) execute() {
-	if c.validArgs() {
-		if *c.before != "" {
-			space, err := c.cliConnection.GetCurrentSpace()
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
+    if c.validArgs() {
+        if *c.before != "" {
+            space, err := c.cliConnection.GetCurrentSpace()
+            if err != nil {
+                fmt.Println(err.Error())
+                return
+            }
 
-			var before time.Time
-			if *c.before == "now" {
-				before = time.Now()
-			} else {
-				before, err = time.Parse(time.RFC3339, *c.before)
-				if err != nil {
-					fmt.Println(err.Error())
-					return
-				}
-			}
-			c.findAppsBefore(space.Guid, before)
-		}
+            var before time.Time
+            if *c.before == "now" {
+                before = time.Now()
+            } else {
+                before, err = time.Parse(time.RFC3339, *c.before)
+                if err != nil {
+                    fmt.Println(err.Error())
+                    return
+                }
+            }
+            c.findAppsBefore(space.Guid, before)
+        }
 
-	} else {
-	}
+    } else {
+    }
 }
 
 func (c *JanitorPlugin) validArgs() bool {
-	return (c.hasFlag(*c.before, "before"))
+    return (c.hasFlag(*c.before, "before"))
 }
 
 func (s *JanitorPlugin) hasFlag(fl string, name string) (ret bool) {
-	if ret = (fl != ""); ret == false {
-		return
-	}
-	return
+    if ret = (fl != ""); ret == false {
+        return
+    }
+    return
 }
 
 /*
@@ -94,54 +94,54 @@ func (s *JanitorPlugin) hasFlag(fl string, name string) (ret bool) {
 *	to the user in the core commands `cf help`, `cf`, or `cf -h`.
  */
 func (c *JanitorPlugin) GetMetadata() plugin.PluginMetadata {
-	return plugin.PluginMetadata{
-		Name: "Janitor",
-		Version: plugin.VersionType{
-			Major: 1,
-			Minor: 0,
-			Build: 0,
-		},
-		MinCliVersion: plugin.VersionType{
-			Major: 6,
-			Minor: 7,
-			Build: 0,
-		},
-		Commands: []plugin.Command{
-			plugin.Command{
-				Name:     "janitor",
-				HelpText: "Janitor command's help text",
+    return plugin.PluginMetadata{
+        Name: "Janitor",
+        Version: plugin.VersionType{
+            Major: 1,
+            Minor: 0,
+            Build: 0,
+        },
+        MinCliVersion: plugin.VersionType{
+            Major: 6,
+            Minor: 7,
+            Build: 0,
+        },
+        Commands: []plugin.Command{
+            plugin.Command{
+                Name:     "janitor",
+                HelpText: "Janitor command's help text",
 
-				// UsageDetails is optional
-				// It is used to show help of usage of each command
-				UsageDetails: plugin.Usage{
-					Usage: "janitor",
-				},
-			},
-		},
-	}
+                // UsageDetails is optional
+                // It is used to show help of usage of each command
+                UsageDetails: plugin.Usage{
+                    Usage: "janitor",
+                },
+            },
+        },
+    }
 }
 
 
 func (c* JanitorPlugin) findAppsBefore(spaceGuid string, before time.Time) {
 
-	appCmd := []string{"curl", "/v2/spaces/" + spaceGuid + "/apps"}
-	appsJson, err := c.cliConnection.CliCommandWithoutTerminalOutput(appCmd...)
+    appCmd := []string{"curl", "/v2/spaces/" + spaceGuid + "/apps"}
+    appsJson, err := c.cliConnection.CliCommandWithoutTerminalOutput(appCmd...)
 
-	if err != nil {
-		return
-	}
+    if err != nil {
+        return
+    }
 
-	res := &resources.PaginatedApplicationResources{}
-	json.Unmarshal([]byte(strings.Join(appsJson,"")), &res)
+    res := &resources.PaginatedApplicationResources{}
+    json.Unmarshal([]byte(strings.Join(appsJson,"")), &res)
 
-	for _,appRes := range res.Resources {
-		appName    	   := *appRes.Entity.Name
-		lastUploadStr  := fmt.Sprint(appRes.Entity.PackageUpdatedAt)
-		lastUploadTime := *appRes.Entity.PackageUpdatedAt
-		if lastUploadTime.Before(before) {
-			fmt.Println(appName + " last uploaded " + lastUploadStr)
-		}
-	}
+    for _,appRes := range res.Resources {
+        appName    	   := *appRes.Entity.Name
+        lastUploadStr  := fmt.Sprint(appRes.Entity.PackageUpdatedAt)
+        lastUploadTime := *appRes.Entity.PackageUpdatedAt
+        if lastUploadTime.Before(before) {
+            fmt.Println(appName + " last uploaded " + lastUploadStr)
+        }
+    }
 }
 
 /*
@@ -151,15 +151,15 @@ func (c* JanitorPlugin) findAppsBefore(spaceGuid string, before time.Time) {
 * plugin.
  */
 func main() {
-	// Any initialization for your plugin can be handled here
-	//
-	// Note: to run the plugin.Start method, we pass in a pointer to the struct
-	// implementing the interface defined at "github.com/cloudfoundry/cli/plugin/plugin.go"
-	//
-	// Note: The plugin's main() method is invoked at install time to collect
-	// metadata. The plugin will exit 0 and the Run([]string) method will not be
-	// invoked.
-	plugin.Start(new(JanitorPlugin))
-	// Plugin code should be written in the Run([]string) method,
-	// ensuring the plugin environment is bootstrapped.
+    // Any initialization for your plugin can be handled here
+    //
+    // Note: to run the plugin.Start method, we pass in a pointer to the struct
+    // implementing the interface defined at "github.com/cloudfoundry/cli/plugin/plugin.go"
+    //
+    // Note: The plugin's main() method is invoked at install time to collect
+    // metadata. The plugin will exit 0 and the Run([]string) method will not be
+    // invoked.
+    plugin.Start(new(JanitorPlugin))
+    // Plugin code should be written in the Run([]string) method,
+    // ensuring the plugin environment is bootstrapped.
 }
